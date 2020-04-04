d7cab29af47d5a8a15ff747f6d1f27a1d377eed5
Camilo Vargas Antiguay <vargas.antiguay@gmail.com>
2020-04-04 16:38:52 -0300

    Add main index.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_04_184337) do
+ActiveRecord::Schema.define(version: 2020_04_04_190237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,9 @@ ActiveRecord::Schema.define(version: 2020_04_04_184337) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "published_at"
+    t.boolean "deprecated", default: false
+    t.date "deprecated_at"
   end
 
   create_table "orders_of_items", force: :cascade do |t|
