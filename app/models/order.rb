28df8aa38aaf293c57c34306535e29c4a139e504
Camilo Vargas Antiguay <vargas.antiguay@gmail.com>
2020-04-04 18:43:29 -0300

    Add deprecation style.

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ApplicationRecord
   has_many :item_orders, dependent: :destroy
   has_many :sources, through: :order_sources
   has_many :items, through: :item_orders
+  default_scope -> { order(:deprecated, published_at: :desc) }
+  scope :actives, -> { where(deprecated: false) }
+  scope :deprecated, -> { where(deprecated: true) }
 end
