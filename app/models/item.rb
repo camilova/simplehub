986aedd93b05ff693fe0fbbfd05f9065c3e7c805
Camilo Vargas Antiguay <vargas.antiguay@gmail.com>
2020-04-05 13:14:35 -0400

    Add categories associations.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   has_many :item_orders, dependent: :destroy
   has_many :orders, through: :item_orders
+  has_many :item_categories
+  has_many :categories, through: :item_categories
 end
