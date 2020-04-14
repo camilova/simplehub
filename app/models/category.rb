de8462d4fcdc445a3d34bf794661ed65a70a0e92
Camilo Vargas Antiguay <vargas.antiguay@gmail.com>
2020-04-14 16:10:55 -0400

    Filter default category on edition of items.

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,7 @@ class Category < ApplicationRecord
   has_many :item_categories, dependent: :nullify
   has_many :items, through: :item_categories
   default_scope { order(:created_at) }
+  scope :selectable, -> { where('id > ?', Category.default.id) }
 
   def self.default
     # First category will always be static and its defaults to "All"
